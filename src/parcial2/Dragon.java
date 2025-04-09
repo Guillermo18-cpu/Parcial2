95e29e992aeb75be811ea50fc8739cae08ad067e
Samuel <samuelbeltran048@gmail.com>
2025-04-08 20:51:23 -0500

    Se modificaron la clase Guerrero y Dragon

--- src/parcial2/Dragon.java
+++ src/parcial2/Dragon.java
@@ -1,10 +1,11 @@
-
 package parcial2;
 
+public abstract class Dragon extends Criatura {
 
-public abstract class Dragon extends Criatura{
+    //Se creo una variable privada tipo String.
     private String escamas;
 
+    //Se creo el metodo constructor atrayendo las variables de la clase "Criatura" junto con la variable "arma" de la clase "Dragon".
     public Dragon(String escamas, String nombre, int salud, int fuerza) {
         super(nombre, salud, fuerza);
         this.escamas = escamas;
@@ -18,8 +19,19 @@ public abstract class Dragon extends Criatura{
         this.escamas = escamas;
     }
 
+    //se sobreescribe el metodo defender
+    @Override
+    public void defender(int daño) {
+        super.setSalud(super.getSalud() - daño);
+        System.out.println(getNombre() + " intenta bloquear el ataque con sus escamas, pero no lo logra y recibe " + daño + " de daño.");
+    }
 
-    
-    
+    //se sobreescribe el metodo atacar, recordemos que la criatura es dragon, por ende su daño se duplica
+    @Override
+    public void atacar(Criatura objetivo) {
+        int daño = getFuerza() * 2;
+        System.out.println(getNombre() + " lanza un aliento de fuego a " + objetivo.getNombre());
+        objetivo.defender(daño);
+    }
 
 }
