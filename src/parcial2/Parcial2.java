8702e05d3b364b15479df3b08a90afede6a68771
Samuel <samuelbeltran048@gmail.com>
2025-04-08 23:49:52 -0500

    Se modificaron algunos mensajes de las subclases y en la clase prueba se realizo una forma para resetear la salud de las criaturas

--- src/parcial2/Parcial2.java
+++ src/parcial2/Parcial2.java
@@ -9,16 +9,19 @@ public class Parcial2 {
         
         System.out.println("---------Batalla 1----------");
         simularBatalla(dragon, mago); // Aqui lo que esta haciendo es el metodo simular batalla es agrupar las criaturas para generar las batallas entre ellas
+        dragon.setSalud(100);
+        mago.setSalud(100);
         System.out.println("\n");
         System.out.println("---------Batalla 2----------");
         simularBatalla(mago, guerrero);
+        guerrero.setSalud(100);
         System.out.println("\n");
         System.out.println("---------Batalla 3----------");
         simularBatalla(dragon, guerrero);
     }
 
     public static void simularBatalla(Criatura c1, Criatura c2) {
-        System.out.println("\nBatalla entre " + c1.getNombre() + " y " + c2.getNombre());
+        System.out.println("\n----------Batalla entre " + c1.getNombre() + " y " + c2.getNombre()+"----------");
 
         while (c1.estaViva() && c2.estaViva()) {
             c1.atacar(c2);
@@ -26,11 +29,11 @@ public class Parcial2 {
                 c2.atacar(c1);
             }
         }
-
+        
         if (c1.estaViva()) {
-            System.out.println(c1.getNombre() + " ha ganado la batalla con " + c1.getSalud() + " de salud restante.");
+            System.out.println("----------"+c1.getNombre() + " ha ganado la batalla con " + c1.getSalud() + " de salud restante.----------");
         } else {
-            System.out.println(c2.getNombre() + " ha ganado la batalla con " + c2.getSalud() + " de salud restante.");
+            System.out.println("----------"+c2.getNombre() + " ha ganado la batalla con " + c2.getSalud() + " de salud restante.----------");
         }
     }
 }
